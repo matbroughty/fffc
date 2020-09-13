f109007a05d5001822c5648b38870f3d036455e5
matbroughton <mat@broughty.com>
2020-09-13 08:39:14 +0100

    content-type on s3 insert

--- tools/src/main/kotlin/org/broughty/fffc/lambda/S3HtmlGenerator.kt
+++ tools/src/main/kotlin/org/broughty/fffc/lambda/S3HtmlGenerator.kt
@@ -4,6 +4,7 @@ import com.amazonaws.AmazonServiceException
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.model.GetObjectRequest
+import com.amazonaws.services.s3.model.ObjectMetadata
 import org.broughty.fffc.parser.FOUR_FOLD_HPD_FTL
 import org.broughty.fffc.parser.FOUR_FOLD_KENT_FTL
 import org.broughty.fffc.parser.FourFoldFilesCreator
@@ -12,12 +13,13 @@ import org.broughty.fffc.parser.model.FourFoldKent
 import java.io.InputStream
 import java.time.LocalDateTime
 
+
 class S3HtmlGenerator {
 
   fun generate(bucketName: String = "fourfold.co.uk",
                hpdFourFoldData: String = "data/hpd.csv",
-               kentFourFoldData: String = "data/kent.csv",
-               premKillerData: String = "data/killer.csv"
+               kentFourFoldData: String = "data/kent.csv"
+               //premKillerData: String = "data/killer.csv"
   ): String {
     print("bucket = $bucketName and file = $hpdFourFoldData")
     val s3Client = AmazonS3ClientBuilder.defaultClient() as AmazonS3Client
@@ -33,7 +35,7 @@ class S3HtmlGenerator {
 
 
 
-    val hpdHtml = FourFoldFilesCreator().createFiles<FourFoldHpd>(inputStream = hpdFourFoldDataStream,dataFileName = "data/hpd.csv", htmlFileName = "hpd.html", templateName = FOUR_FOLD_HPD_FTL, writeToFile = false)
+    val hpdHtml = FourFoldFilesCreator().createFiles<FourFoldHpd>(inputStream = hpdFourFoldDataStream, dataFileName = "data/hpd.csv", htmlFileName = "hpd.html", templateName = FOUR_FOLD_HPD_FTL, writeToFile = false)
     val kentHtml = FourFoldFilesCreator().createFiles<FourFoldKent>(inputStream = kentFourFoldDataStream, dataFileName = "data/kent.csv", htmlFileName = "index.html", templateName = FOUR_FOLD_KENT_FTL, writeToFile = false)
 
     val hpdFourFoldMsg = createHtmlFile(hpdHtml, bucketName, s3Client, kentFourFoldData, "hpd.html")
@@ -49,8 +51,10 @@ class S3HtmlGenerator {
                              s3Client: AmazonS3Client, dataFile: String, htmlFileName: String): String {
     println("Writing to: $bucketName with html $fileContents")
     var msg = "S3 S3HtmlGenerator"
+    val metadata = ObjectMetadata()
+    metadata.contentType = "text/html"
     try {
-      s3Client.putObject(bucketName, htmlFileName, fileContents)
+      s3Client.putObject(bucketName, htmlFileName, fileContents.byteInputStream(), metadata)
     } catch (e: AmazonServiceException) {
       System.err.println("We have an error writing $dataFile to $bucketName with html $fileContents error is:  ${e.errorMessage}")
       return msg.plus(" FAILED to update $bucketName using $dataFile and uploaded to $bucketName with object $htmlFileName}")
@@ -61,3 +65,5 @@ class S3HtmlGenerator {
   }
 
 }
+
+
