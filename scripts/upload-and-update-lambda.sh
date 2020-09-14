8b69a0c332c531bb38662af6533ecc7ed8eed82d
matbroughton <mat@broughty.com>
2020-09-14 11:35:31 +0100

    updated jar

--- scripts/upload-and-update-lambda.sh
+++ scripts/upload-and-update-lambda.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 aws s3 cp ../tools/build/libs/tools-1.0.0-all.jar s3://fourfold.co.uk --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 aws lambda update-function-code --function-name html-generator-fourfold --s3-bucket fourfold.co.uk --s3-key tools-1.0.0-all.jar
-aws cloudfront create-invalidation --distribution-id E1LNB25U119JXK --paths "/*"
