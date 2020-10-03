32d45b971b3aab24e3dea76dcdd4a77c7353ce6d
matbroughton <mat@broughty.com>
2020-10-03 10:33:54 +0100

    updated killer-hpd.ftl and surrounding logic to cater for multiple games

--- tools/src/main/kotlin/org/broughty/fffc/parser/KillerFileCreator.kt
+++ tools/src/main/kotlin/org/broughty/fffc/parser/KillerFileCreator.kt
@@ -4,6 +4,9 @@ import com.opencsv.bean.CsvToBeanBuilder
 import org.broughty.fffc.parser.model.Killer
 import org.slf4j.LoggerFactory
 import java.io.*
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.memberProperties
+import kotlin.streams.toList
 
 class KillerFileCreator {
 
@@ -13,12 +16,31 @@ class KillerFileCreator {
       if (inputStream != null) CsvToBeanBuilder(InputStreamReader(inputStream)) else {
         CsvToBeanBuilder(FileReader(dataFileName))
       }
-    val rounds: List<Killer> = csvToBeanBuilder.withType(Killer::class.java)
+    val fileRounds: List<Killer> = csvToBeanBuilder.withType(Killer::class.java)
       .withSkipLines(1).withIgnoreEmptyLine(true).build().parse()
-    rounds.forEach { logger.info("Read in Killer Week {}", it) }
+    fileRounds.forEach { logger.info("Read in Killer Week {}", it) }
+
+    // break file up into killer rounds via the '---' separator
+    val rounds : ArrayList<List<Killer>> = ArrayList();
+    var round : ArrayList<Killer> = ArrayList();
+    fileRounds.forEach{
+      if (it.mat.startsWith("---")){
+        rounds.add(round);
+        round = ArrayList()
+      }else{
+        round.add(it)
+      }
+    }
+    rounds.add(round)
+
+    logger.info("We have {} rounds", rounds.size)
+
+    // List of players from field names - bit ugly...
+    val players : List<String> = Killer::class.memberProperties.stream().map { it.name }.toList()
+
     val template = FreeMarkerUtils().getFreeMarker(templateName)
     val htmlStr = StringWriter()
-    template.process(mapOf(Pair("rounds", rounds)), htmlStr)
+    template.process(mapOf(Pair("allRounds", rounds), Pair("players", players)), htmlStr)
     logger.debug("Killer html {}", htmlStr)
     val file = File(htmlFileName)
     if (writeToFile) {
--- tools/src/main/kotlin/org/broughty/fffc/parser/model/Killer.kt
+++ tools/src/main/kotlin/org/broughty/fffc/parser/model/Killer.kt
@@ -9,7 +9,6 @@ data class Killer(@CsvBindByPosition(position = 0) val mat: String = "",
              @CsvBindByPosition(position = 4) val jon: String = "",
              @CsvBindByPosition(position = 5) val frank: String = "",
              @CsvBindByPosition(position = 6) val dave: String = "",
-             @CsvBindByPosition(position = 7) val tim: String = "") {
-
-
-}
+             @CsvBindByPosition(position = 7) val tim: String = "",
+             @CsvBindByPosition(position = 8) val jesus: String = ""
+)
